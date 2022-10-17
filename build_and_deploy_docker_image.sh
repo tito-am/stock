9c4546c22774a0d6b60cdc60e3b6ed6074a79404
tito arandia <fabian.arandia@gmail.com>
2022-10-17 19:29:42 -0400

    changement sh

--- build_and_deploy_docker_image.sh
+++ build_and_deploy_docker_image.sh
@@ -1,6 +1,6 @@
 # get project-ID:
 #gcloud config get-value project
-PROJECT_ID='stock-ycng-228'
+PROJECT_ID='stock-ftam'
 ### build docker and push into GCP
 gcloud builds submit --tag gcr.io/${PROJECT_ID}/stockpredictor
 

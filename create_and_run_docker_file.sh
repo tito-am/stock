4e5dbe347a652dc7b47bbc2a6005cf27965c95a7
tito arandia <fabian.arandia@gmail.com>
2022-10-18 19:25:49 -0400

    ajout credentials

--- create_and_run_docker_file.sh
+++ create_and_run_docker_file.sh
@@ -1,6 +1,6 @@
 #for local testing, you can build and run your docker image locally
 docker build . -f Dockerfile -t my_image
-
+export GOOGLE_APPLICATION_CREDENTIALS='/Users/caramelo/Downloads/stock-ftam-01df728fdebc-labonne.json'
 #to run the docker image:
 docker run -p 8080:8080 -v $GOOGLE_APPLICATION_CREDENTIALS:/creds.json -e GOOGLE_APPLICATION_CREDENTIALS=/creds.json my_image
 #to log into the docker image:
